1449ca63e69f9b57650f2a1fed40488e328f9d43
Maruf Hossain <mhtamun@gmail.com>
2023-06-13 08:25:57 +0600

    Delete coreUI theme

--- src/api/auth.js
+++
@@ -1,6 +0,0 @@
-import { callPostApi } from '../libs/api';
-import { apiBaseUrl } from '../data/env';
-
-export const login = (payload) => callPostApi(apiBaseUrl + '/api/v1/auth/sign-in', payload);
-
-export const resetPassword = () => {};
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import sidebarSliceReducer from './sidebarSlice';
-import apiCallSliceReducer from './apiCallSlice';
-
-export const store = configureStore({
-    reducer: { sidebar: sidebarSliceReducer, apiCall: apiCallSliceReducer },
-});
